e7089fd995e3cdec4e1b4d933d62436b7f1bbe37
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-08-16 17:19:29 +0500

    1.setting Constants for App

---
+++ lib/utils/constants/texts.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+
+class Text {
+
+  // OnBoardingScreen
+   static const String onBoardingTitle1 ='Choose Your Product';
+   static const String onBoardingTitle2 ='Select Payment Method';
+   static const String onBoardingTitle3 ='Deliver at your door step';
+
+   static const String onBoardingSubTitle1 ='Welcome to a world of Limitless Choices - Your Perfect Products Awaits!';
+   static const String onBoardingSubTitle2 ='For Seamless Transcations,Choose Your Payment Path - Your Conveinence,Our Priority !';
+   static const String onBoardingSubTitle3 ='From Our Doorstep to Yours - Swift,Secure,and Contactless Delivery! ';
+
+
+   // Home
+
+ static const String homeAppbarTitle = 'Good Day For Shopping ' ;
+
+
+}
