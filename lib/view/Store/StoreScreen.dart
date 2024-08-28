d387e0a0dc67f3eecc2f68ebdcb5190a0e1622eb
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-08-28 14:52:09 +0500

    1.Making Ecommerce app

---
+++ lib/view/Store/StoreScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class StoreScreen extends StatefulWidget {
+  const StoreScreen({super.key});
+
+  @override
+  State<StoreScreen> createState() => _StoreScreenState();
+}
+
+class _StoreScreenState extends State<StoreScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
