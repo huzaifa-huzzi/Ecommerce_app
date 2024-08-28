d75176c24cd9cc7e5bdceed4a179c09dfd41e5b0
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-08-28 15:51:03 +0500

    1.Making Ecommerce app

--- lib/view/Home/HomeScreen.dart
+++ lib/view/Home/HomeScreen.dart
@@ -1,3 +1,5 @@
+import 'package:ecommerece_app/Resources/Components/circularContainer/CircularContainer.dart';
+import 'package:ecommerece_app/utils/constants/Colors.dart';
 import 'package:flutter/material.dart';
 
 
@@ -12,6 +14,30 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ClipPath(
+              clipper: ,
+              child: Container(
+                color: AppColor.primary,
+                padding:const EdgeInsets.all(0),
+                child: SizedBox(
+                  height: 400,
+                  child: Stack(
+                    children: [
+                      Positioned(top: -150,right: -230,child: CircularContainer(backgroundColor: AppColor.textWhite.withOpacity(0.1),)),
+                      Positioned(top: 100,right: -310,child: CircularContainer(backgroundColor: AppColor.textWhite.withOpacity(0.1))),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
+
