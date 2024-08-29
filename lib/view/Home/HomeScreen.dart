7feb888a14660d3a32451341ec4576e03d6fec82
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-08-29 16:36:23 +0500

    1.Making Ecommerce app

--- lib/view/Home/HomeScreen.dart
+++ lib/view/Home/HomeScreen.dart
@@ -3,11 +3,8 @@ import 'package:ecommerece_app/Resources/Components/circularContainer/CircularCo
 import 'package:ecommerece_app/Resources/Components/curvedEdges.dart/curevedEdges.dart';
 import 'package:ecommerece_app/utils/constants/Colors.dart';
 import 'package:ecommerece_app/utils/constants/sizes.dart';
-import 'package:ecommerece_app/utils/constants/texts.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
-
-
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -28,42 +25,61 @@ class _HomeScreenState extends State<HomeScreen> {
               clipper: CurvedEdges(),
               child: Container(
                 color: AppColor.primary,
-                padding:const EdgeInsets.all(0),
+                padding: const EdgeInsets.all(0),
                 child: SizedBox(
                   height: 400,
                   child: Stack(
                     children: [
-                      Positioned(top: -150,right: -230,child: CircularContainer(backgroundColor: AppColor.textWhite.withOpacity(0.1),)),
-                      Positioned(top: 100,right: -310,child: CircularContainer(backgroundColor: AppColor.textWhite.withOpacity(0.1))),
+                      Positioned(
+                        top: -150,
+                        right: -230,
+                        child: CircularContainer(
+                          backgroundColor: AppColor.textWhite.withOpacity(0.1),
+                        ),
+                      ),
+                      Positioned(
+                        top: 100,
+                        right: -310,
+                        child: CircularContainer(
+                          backgroundColor: AppColor.textWhite.withOpacity(0.1),
+                        ),
+                      ),
                       HomeAppBarWidget(),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal:Sizes.defaultSpace),
-                        child: Container(
-                          width: MediaQuery.sizeOf(context).width * .9,
-                          height: MediaQuery.sizeOf(context).width * .02,
-                          padding: EdgeInsets.all(Sizes.md),
-                          decoration: BoxDecoration(
-                              color: Colors.transparent,
+                      /// Centering the search bar vertically in the available space
+                      Align(
+                        alignment: Alignment.center,
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: Sizes.defaultSpace),
+                          child: Container(
+                            width: MediaQuery.of(context).size.width * 0.9,
+                            height: MediaQuery.of(context).size.width * 0.15,
+                            padding: EdgeInsets.all(Sizes.md),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
                               borderRadius: BorderRadius.circular(Sizes.cardRadiusLg),
-                              border: Border.all(color: AppColor.grey)
+                              border: Border.all(color: AppColor.grey),
+                            ),
+                            child: const Row(
+                              children: [
+                                Icon(Iconsax.search_normal, color: AppColor.darkerGrey),
+                                SizedBox(width: Sizes.defaultSpace),
+                                Text(
+                                  "Search in store",
+                                  style: TextStyle(color: AppColor.darkerGrey),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
                       ),
-                      const  SizedBox(height:Sizes.spaceBtwItems ,),
-                       /// searchBar
-
-
                     ],
-
                   ),
                 ),
-
               ),
-            )
+            ),
           ],
         ),
       ),
     );
   }
 }
-
