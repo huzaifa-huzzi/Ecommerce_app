108ef90ea077c771c6b079b6acae07502ee8a0fa
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-09-03 20:56:09 +0500

    1.Making Ecommerce app

--- lib/Resources/Components/HomeAppBar/HomeAppBarWidget.dart
+++ lib/Resources/Components/HomeAppBar/HomeAppBarWidget.dart
@@ -1,5 +1,4 @@
 import 'package:ecommerece_app/utils/constants/Colors.dart';
-import 'package:ecommerece_app/utils/constants/sizes.dart';
 import 'package:ecommerece_app/utils/constants/texts.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
