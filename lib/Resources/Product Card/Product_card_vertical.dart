fd84369c1fa4ba52191d897e84bd928dd5c3973b
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-09-04 17:00:19 +0500

    1.Making Ecommerce app

--- lib/Resources/Product Card/Product_card_vertical.dart
+++ lib/Resources/Product Card/Product_card_vertical.dart
@@ -1,96 +1,108 @@
-import 'package:ecommerece_app/Resources/Components/FavouriteIcon/FavouriteIcon.dart';
-import 'package:ecommerece_app/Resources/Components/RoundedContainer/RoundedContainer.dat.dart';
-import 'package:ecommerece_app/utils/constants/Colors.dart';
-import 'package:ecommerece_app/utils/constants/sizes.dart';
-import 'package:ecommerece_app/utils/shadow/ShadowStyle.dart';
 import 'package:ecommerece_app/view_model/Controller/ThemeController/ThemeController.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 
 class ProductCardVertical extends StatelessWidget {
-  const ProductCardVertical({super.key});
+  const ProductCardVertical({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final ThemeController themeController = Get.put(ThemeController());
     return Container(
       width: 180,
-      padding: EdgeInsets.all(1),
       decoration: BoxDecoration(
-        boxShadow: [ShadowStyle.verticalProductShadow],
-        borderRadius: BorderRadius.circular(Sizes.productImageRadius),
-        color: themeController.isDarkTheme.value ? AppColor.darkerGrey : Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 10.0,
+            offset: Offset(0, 10),
+          ),
+        ],
+        borderRadius: BorderRadius.circular(16.0),
+        color: themeController.isDarkTheme.value
+            ? Colors.grey[800]
+            : Colors.white,
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           /// Thumbnail, Wishlist, Discount tag
-          RoundedContainer(
+          Container(
             height: 180,
-            padding: EdgeInsets.all(Sizes.sm),
-            backgroundColor: themeController.isDarkTheme.value ? AppColor.dark : AppColor.light,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(16.0),
+                topRight: Radius.circular(16.0),
+              ),
+              color: themeController.isDarkTheme.value
+                  ? Colors.grey[700]
+                  : Colors.grey[200],
+            ),
             child: Stack(
               children: [
-                /// -- Thumbnail Image
-              Container(
-              width: 180,
-              height: 200,
-              padding: EdgeInsets.all(1),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8.0),
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 2,
-                    blurRadius: 5,
-                    offset: Offset(0, 3),
-                  ),
-                ],
-              ),
-              child: Image.asset(
-                'assets/banners/banner-1.png', // Make sure this path is correct
-                fit: BoxFit.cover,
-                errorBuilder: (context, error, stackTrace) {
-                  return Icon(
-                    Icons.broken_image,
-                    size: 50,
-                    color: Colors.grey,
-                  ); // Display an error icon if the image fails to load
-                },
-              ),
-            ),
-
-                /// -- Sale Tag
-                Positioned(
-                  top: 12,
-                  left: 12,
-                  child: RoundedContainer(
-                    radius: Sizes.sm,
-                    backgroundColor: AppColor.secondary.withOpacity(0.8),
-                    padding: const EdgeInsets.symmetric(horizontal: Sizes.sm, vertical: Sizes.xs),
-                    child: Text(
-                      '25%',
-                      style: Theme.of(context).textTheme.labelLarge!.apply(color: AppColor.black),
+                /// Thumbnail Image
+                Align(
+                  alignment: Alignment.center, // Centers the image within the container
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 25.0), // Adjust padding to position the image vertically
+                    child: Image.asset(
+                      'assets/banners/banner-3.png', // Ensure this path is correct
+                      width: 150,
+                      height: 120,
+                      fit: BoxFit.contain, // Ensures the image maintains its aspect ratio
+                      errorBuilder: (context, error, stackTrace) {
+                        return Center(
+                          child: Icon(Icons.error, color: Colors.red),
+                        );
+                      },
                     ),
                   ),
                 ),
 
-                /// -- Favourite Icon
+                /// Sale Tag
+                Positioned(
+                  top: 12,
+                  left: 12,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 8.0,
+                      vertical: 4.0,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.yellow.withOpacity(0.8),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: Text(
+                      '25%',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+
+                /// Favourite Icon
                 Positioned(
                   top: 12,
                   right: 12,
-                  child: FavuriteIconButton(
-                    themeController: themeController,
-                    icon: Iconsax.heart5,
+                  child: IconButton(
+                    icon: Icon(
+                      Iconsax.heart5,
+                      color: themeController.isDarkTheme.value
+                          ? Colors.white
+                          : Colors.red,
+                    ),
+                    onPressed: () {
+                      // Handle the favourite button press
+                    },
                   ),
                 ),
               ],
             ),
           ),
-
-          /// Product Details (if any) can be added here.
+          // You can add more details below the image as needed
         ],
       ),
     );
