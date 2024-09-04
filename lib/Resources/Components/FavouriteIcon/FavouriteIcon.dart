efaeb170229e921f648e8b15fa22c809d7fae970
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-09-04 16:20:46 +0500

    1.Making Ecommerce app

---
+++ lib/Resources/Components/FavouriteIcon/FavouriteIcon.dart
@@ -0,0 +1,39 @@
+import 'package:ecommerece_app/utils/constants/Colors.dart';
+import 'package:ecommerece_app/utils/constants/sizes.dart';
+import 'package:ecommerece_app/view_model/Controller/ThemeController/ThemeController.dart';
+import 'package:flutter/material.dart';
+
+
+class FavuriteIconButton extends StatelessWidget {
+  const FavuriteIconButton({
+    super.key,
+    required this.themeController,
+    this.width,
+    this.height,
+    this.size = Sizes.lg,
+    this.onPressed,
+    this.color,
+    this.backgroundColor, required this.icon
+  });
+
+  final ThemeController themeController;
+  final double? width,height,size;
+  final IconData icon;
+  final Color? backgroundColor;
+  final Color? color;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100),
+        color:backgroundColor != null ? backgroundColor : themeController.isDarkTheme.value ? AppColor.black.withOpacity(0.9) : Colors.white.withOpacity(0.9),
+      ),
+      child: IconButton(onPressed:onPressed, icon:  Icon(icon)),
+    );
+  }
+}
+
--- lib/Resources/Product Card/Product_card_vertical.dart
+++ lib/Resources/Product Card/Product_card_vertical.dart
@@ -1,9 +1,13 @@
+import 'package:ecommerece_app/Resources/Components/FavouriteIcon/FavouriteIcon.dart';
+import 'package:ecommerece_app/Resources/Components/RoundedContainer/RoundedContainer.dat.dart';
+import 'package:ecommerece_app/Resources/Components/circularContainer/CircularContainer.dart';
 import 'package:ecommerece_app/utils/constants/Colors.dart';
 import 'package:ecommerece_app/utils/constants/sizes.dart';
 import 'package:ecommerece_app/utils/shadow/ShadowStyle.dart';
 import 'package:ecommerece_app/view_model/Controller/ThemeController/ThemeController.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
 
 
 
@@ -24,7 +28,42 @@ class ProductCardVertical extends StatelessWidget {
       child: Column(
         children: [
             /// Thumnail,Wishlisht,Discount tag
-
+            RoundedContainer(
+              height: 180,
+              padding: EdgeInsets.all(Sizes.sm),
+              backgroundColor: themeController.isDarkTheme.value ? AppColor.dark : AppColor.light,
+              child: Stack(
+                children: [
+                  /// -- Thumbnail Image
+                  Container(
+                    width: 320,
+                    height: 200,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(Sizes.md),
+                      child: Image.asset(
+                        'assets/banners/banner-1.png',
+                        fit: BoxFit.contain, // or BoxFit.fill
+                      ),
+                    ),
+                  ),
+                  /// -- sale Tag
+                  Positioned(
+                    top: 12,
+                    child: RoundedContainer(
+                      radius: Sizes.sm,
+                      backgroundColor: AppColor.secondary.withOpacity(0.8),
+                      padding:const  EdgeInsets.symmetric(horizontal: Sizes.sm,vertical: Sizes.xs),
+                      child: Text('25%',style: Theme.of(context).textTheme.labelLarge!.apply(color: AppColor.black),),
+                    ),
+                  ),
+                  /// Favourite Icon
+                 Positioned(
+                     top: 0,
+                     right: 0,
+                     child: FavuriteIconButton(themeController: themeController, icon: Iconsax.heart5))
+                ],
+              ),
+            )
 
            /// Details
 
@@ -34,3 +73,4 @@ class ProductCardVertical extends StatelessWidget {
     );
   }
 }
+
