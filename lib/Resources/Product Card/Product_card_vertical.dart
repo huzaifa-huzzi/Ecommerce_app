e17a67cbd6aa1024fb1d6d796282a7694c409ed4
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-09-04 16:39:22 +0500

    1.Making Ecommerce app

--- lib/Resources/Product Card/Product_card_vertical.dart
+++ lib/Resources/Product Card/Product_card_vertical.dart
@@ -1,6 +1,5 @@
 import 'package:ecommerece_app/Resources/Components/FavouriteIcon/FavouriteIcon.dart';
 import 'package:ecommerece_app/Resources/Components/RoundedContainer/RoundedContainer.dat.dart';
-import 'package:ecommerece_app/Resources/Components/circularContainer/CircularContainer.dart';
 import 'package:ecommerece_app/utils/constants/Colors.dart';
 import 'package:ecommerece_app/utils/constants/sizes.dart';
 import 'package:ecommerece_app/utils/shadow/ShadowStyle.dart';
@@ -8,8 +7,6 @@ import 'package:ecommerece_app/view_model/Controller/ThemeController/ThemeContro
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
-
-
 
 class ProductCardVertical extends StatelessWidget {
   const ProductCardVertical({super.key});
@@ -26,51 +23,76 @@ class ProductCardVertical extends StatelessWidget {
         color: themeController.isDarkTheme.value ? AppColor.darkerGrey : Colors.white,
       ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-            /// Thumnail,Wishlisht,Discount tag
-            RoundedContainer(
-              height: 180,
-              padding: EdgeInsets.all(Sizes.sm),
-              backgroundColor: themeController.isDarkTheme.value ? AppColor.dark : AppColor.light,
-              child: Stack(
-                children: [
-                  /// -- Thumbnail Image
-                  Container(
-                    width: 320,
-                    height: 200,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(Sizes.md),
-                      child: Image.asset(
-                        'assets/banners/banner-1.png',
-                        fit: BoxFit.contain, // or BoxFit.fill
-                      ),
-                    ),
+          /// Thumbnail, Wishlist, Discount tag
+          RoundedContainer(
+            height: 180,
+            padding: EdgeInsets.all(Sizes.sm),
+            backgroundColor: themeController.isDarkTheme.value ? AppColor.dark : AppColor.light,
+            child: Stack(
+              children: [
+                /// -- Thumbnail Image
+              Container(
+              width: 180,
+              height: 200,
+              padding: EdgeInsets.all(1),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8.0),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: Offset(0, 3),
                   ),
-                  /// -- sale Tag
-                  Positioned(
-                    top: 12,
-                    child: RoundedContainer(
-                      radius: Sizes.sm,
-                      backgroundColor: AppColor.secondary.withOpacity(0.8),
-                      padding:const  EdgeInsets.symmetric(horizontal: Sizes.sm,vertical: Sizes.xs),
-                      child: Text('25%',style: Theme.of(context).textTheme.labelLarge!.apply(color: AppColor.black),),
-                    ),
-                  ),
-                  /// Favourite Icon
-                 Positioned(
-                     top: 0,
-                     right: 0,
-                     child: FavuriteIconButton(themeController: themeController, icon: Iconsax.heart5))
                 ],
               ),
-            )
+              child: Image.asset(
+                'assets/banners/banner-1.png', // Make sure this path is correct
+                fit: BoxFit.cover,
+                errorBuilder: (context, error, stackTrace) {
+                  return Icon(
+                    Icons.broken_image,
+                    size: 50,
+                    color: Colors.grey,
+                  ); // Display an error icon if the image fails to load
+                },
+              ),
+            ),
 
-           /// Details
+                /// -- Sale Tag
+                Positioned(
+                  top: 12,
+                  left: 12,
+                  child: RoundedContainer(
+                    radius: Sizes.sm,
+                    backgroundColor: AppColor.secondary.withOpacity(0.8),
+                    padding: const EdgeInsets.symmetric(horizontal: Sizes.sm, vertical: Sizes.xs),
+                    child: Text(
+                      '25%',
+                      style: Theme.of(context).textTheme.labelLarge!.apply(color: AppColor.black),
+                    ),
+                  ),
+                ),
 
+                /// -- Favourite Icon
+                Positioned(
+                  top: 12,
+                  right: 12,
+                  child: FavuriteIconButton(
+                    themeController: themeController,
+                    icon: Iconsax.heart5,
+                  ),
+                ),
+              ],
+            ),
+          ),
 
+          /// Product Details (if any) can be added here.
         ],
       ),
     );
   }
 }
-
--- lib/view/Home/HomeScreen.dart
+++ lib/view/Home/HomeScreen.dart
@@ -98,6 +98,7 @@ class _HomeScreenState extends State<HomeScreen> {
            const SizedBox(height: Sizes.defaultSpace,),
            /// body
           const HomeSliderWidget(),
+            const SizedBox(height: Sizes.defaultSpace,),
            const  ProductCardVertical(),
 
 
