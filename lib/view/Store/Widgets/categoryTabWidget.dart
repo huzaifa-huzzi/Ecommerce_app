c31fc95c10ba591d165ef03fc46677add3cde0bc
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-09-09 16:58:51 +0500

    1.Making Ecommerce app

--- lib/view/Store/Widgets/categoryTabWidget.dart
+++ lib/view/Store/Widgets/categoryTabWidget.dart
@@ -14,22 +14,82 @@ class CategoryTabWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final ThemeController themeController = Get.put(ThemeController());
+    bool isDarkTheme = themeController.isDarkTheme.value;
 
     return ListView(
       padding: const EdgeInsets.all(Sizes.defaultSpace),
       children: [
         // Brand Showcase Row
-        BrandShocaseStoreWidget(
-          themeController: themeController,
-          images: [
-            'assets/images/product1.png',
-            'assets/images/product2.png',
-            'assets/images/product3.png',
-          ],
-          productCount: 26,
+        Container(
+          padding: const EdgeInsets.all(Sizes.sm),
+          decoration: BoxDecoration(
+            color: isDarkTheme ? Colors.black : Colors.white, // Background color based on theme
+            borderRadius: BorderRadius.circular(8),
+            border: Border.all(
+              color: isDarkTheme ? Colors.white : Colors.grey.shade800, // Border color based on theme
+              width: 1.5,
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: isDarkTheme ? Colors.black.withOpacity(0.5) : Colors.black.withOpacity(0.1),
+                spreadRadius: 2,
+                blurRadius: 4,
+                offset: const Offset(0, 2), // Changes position of shadow
+              ),
+            ],
+          ),
+          child: Row(
+            children: [
+              /// -- Icon
+              Container(
+                width: 56,
+                height: 56,
+                padding: const EdgeInsets.all(Sizes.sm),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(8), // Maintain shape of the logo
+                  child: Image.asset(
+                    isDarkTheme ? 'assets/logos/dark_nike.png' : 'assets/logos/nike-3.png',
+                    fit: BoxFit.cover,
+                    errorBuilder: (context, error, stackTrace) {
+                      return Container(color: Colors.red);
+                    },
+                  ),
+                ),
+              ),
 
-          
+              const SizedBox(width: Sizes.spaceBtwItems / 2),
+              /// -- Text
+              Expanded(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const BrandNameWithIcon(brandName: 'Nike'),
+                    Text(
+                      '256 Products',
+                      overflow: TextOverflow.ellipsis,
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                  child:  BrandShocaseStoreWidget(
+                    themeController: themeController,
+                    images: [
+                      'assets/images/product1.png',
+                      'assets/images/product2.png',
+                      'assets/images/product3.png',
+                    ],
+                    productCount: 26,
+
+
+                  ),
+              ),
+            ],
+          ),
         ),
+
 
         const SizedBox(height: Sizes.spaceBtwSections),
         // Section Heading
