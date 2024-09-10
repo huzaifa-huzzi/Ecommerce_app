59f000bccb46ca89f1933074d893cfb93cb36ac9
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-09-10 16:14:29 +0500

    1.Making Ecommerce app

--- lib/view/profile/ProfileScreen.dart
+++ lib/view/profile/ProfileScreen.dart
@@ -1,5 +1,12 @@
+import 'package:ecommerece_app/Resources/Components/circularContainer/CircularContainer.dart';
+import 'package:ecommerece_app/Resources/Components/curvedEdges.dart/curevedEdges.dart';
+import 'package:ecommerece_app/utils/constants/Colors.dart';
+import 'package:ecommerece_app/view/profile/widget/ProfileAppBarScreen.dart';
+import 'package:ecommerece_app/view/wishlist/Widgets/WishListAppBarWidget.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
+import '../../utils/constants/sizes.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -9,8 +16,85 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: ClipPath(
+        clipper: CurvedEdges(), // Custom clipper for curved design
+        child: Container(
+          color: AppColor.primary, // Blue color area
+          child: SizedBox(
+            height: 220, // Adjust height as needed
+            child: Stack(
+              children: [
+                // Background circular containers
+                Positioned(
+                  top: -150,
+                  right: -230,
+                  child: CircularContainer(
+                    backgroundColor: AppColor.textWhite.withOpacity(0.1),
+                  ),
+                ),
+                Positioned(
+                  top: 100,
+                  right: -310,
+                  child: CircularContainer(
+                    backgroundColor: AppColor.textWhite.withOpacity(0.1),
+                  ),
+                ),
+
+                /// positioned inside the blue area
+              const  ProfileAppBarWidgetSetInProfile(),
+                  Positioned(
+                    top: 100,
+                    left: 10,
+                    right: 20,
+                    child: Column(
+                      children: [
+                        ListTile(
+                          leading:const  CircleAvatar(
+                            radius: 25,
+                            backgroundImage: AssetImage('assets/images/background@.png'), // Provide the path to your image
+                          ),
+                          title: Text(
+                            'Huzaifa Khan',
+                            style: Theme.of(context).textTheme.headlineSmall!.apply(color: Colors.white),
+                          ),
+                          subtitle: Text(
+                            'support@gmail.com',
+                            style: Theme.of(context).textTheme.bodyMedium!.apply(color: Colors.white),
+                          ),
+                          trailing: IconButton(onPressed: (){}, icon:const  Icon(Iconsax.edit,color: Colors.white,)),
+                        ),
+                        SizedBox(height: Sizes.spaceBtwSections,)
+
+                      ],
+                    )
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
+
+class ProfileAppBarWidgetSetInProfile extends StatelessWidget {
+  const ProfileAppBarWidgetSetInProfile({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const  Positioned(
+       top: 20,
+       left: 20,
+       right: 20,
+       child: const ProfileAppBarScreenWidget(title: 'Account', subtitle: '')
+     );
+  }
+}
+
