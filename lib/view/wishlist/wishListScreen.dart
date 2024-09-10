a277569b29792236bb59f53ebabca3777f587c54
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-09-10 15:42:31 +0500

    1.Making Ecommerce app

--- lib/view/wishlist/wishListScreen.dart
+++ lib/view/wishlist/wishListScreen.dart
@@ -1,4 +1,8 @@
+import 'package:ecommerece_app/Resources/Product%20Card/Product_card_vertical.dart';
+import 'package:ecommerece_app/view/wishlist/Widgets/WishListAppBarWidget.dart';
 import 'package:flutter/material.dart';
+
+import '../../utils/constants/Sizes.dart';
 
 
 class WishlistScreen extends StatefulWidget {
@@ -11,6 +15,26 @@ class WishlistScreen extends StatefulWidget {
 class _WishlistScreenState extends State<WishlistScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: const wishListAppBarWidget(title: 'Wishlist', subtitle:''),
+      body: SingleChildScrollView(
+        child: Padding(
+            padding: const EdgeInsets.all(Sizes.defaultSpace),
+          child: Column(
+            children: [
+              GridView.builder(
+                  itemCount: 4,
+                  shrinkWrap: true,
+                  padding: EdgeInsets.zero,
+                  physics:const  NeverScrollableScrollPhysics(),
+                  gridDelegate:const
+                  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,mainAxisSpacing: Sizes.gridViewSpacing,crossAxisSpacing: Sizes.gridViewSpacing,mainAxisExtent: 288), itemBuilder: (_,index){
+                return const  ProductCardVertical();
+              }),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
