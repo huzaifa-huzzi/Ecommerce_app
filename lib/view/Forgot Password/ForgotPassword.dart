ae9ccb0ef4e217cfc156c666ac8c1e8da3d1d513
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-09-13 14:22:01 +0500

    1.Making Ecommerce app

--- lib/view/Forgot Password/ForgotPassword.dart
+++ lib/view/Forgot Password/ForgotPassword.dart
@@ -6,9 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 
-
-
-
 class ForgotPassword extends StatefulWidget {
   const ForgotPassword({super.key});
 
