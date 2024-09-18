ee0cfecb214368958bea64b1398d01468433a00f
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-09-18 17:14:38 +0500

    1.Making Ecommerce app

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:ecommerece_app/view/splash/SplashScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+
 void main() {
   runApp(const MyApp());
 }
