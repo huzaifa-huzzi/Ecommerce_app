f5106d362b3f4faa7b652d487d0cf7bf60149b35
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-10-16 15:39:17 +0500

    1.Making Ecommerce app

---
+++ lib/view/Adresses/address.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class UserAddress extends StatefulWidget {
+  const UserAddress({super.key});
+
+  @override
+  State<UserAddress> createState() => _UserAddressState();
+}
+
+class _UserAddressState extends State<UserAddress> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
