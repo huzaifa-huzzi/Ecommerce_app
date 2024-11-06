e57e5b58de0c5fd52453134091d33a4210388e9e
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-11-06 15:17:22 +0500

    1.Making Ecommerce app

--- lib/utils/enum/enum.dart
+++
@@ -1,5 +0,0 @@
-enum TextSize{small,medium,large}
-
-enum OrderStatus {processing,shipped,delivered}
-
-enum PaymentMethods {paypal,googlePAy,applePAy,visa,masterCard,creditCard,payStack,razorPay,paytm}
--- lib/view_model/Controller/choiceChipController/choicechipcontroller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class ChoiceChipController extends GetxController {
-  var selectedChipIndex = (-1).obs; // Observable variable for the selected chip index
-
-  void selectChip(int index) {
-    selectedChipIndex.value = index;
-  }
-}
